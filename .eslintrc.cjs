80e9882c1e05092e2596fc3e3d6d1e683ac1e46a
Rick <44267427+rickbairstow@users.noreply.github.com>
2024-08-12 12:30:22 +0100

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,55 +1,56 @@
 module.exports = {
-  root: true,
-  extends: [
-    'plugin:vue/vue3-recommended',
-    'eslint:recommended',
-    'plugin:storybook/recommended',
-    "@vue/eslint-config-prettier"
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  },
-  env: {
-    browser: true,
-    amd: true,
-    node: true
-  },
-  rules: {
-    eqeqeq: ['error', 'always'],
-    'vue/eqeqeq': ['error', 'always'],
-    'prettier/prettier':
-      process.env.NODE_ENV === 'production' ? 'error' : 'warn',
-    'no-unused-vars':
-      process.env.NODE_ENV === 'production' ? 'error' : 'warn',
-    'vue/no-unused-vars':
-      process.env.NODE_ENV === 'production' ? 'error' : 'warn',
-    'vue/attributes-order': [
-      'warn',
-      {
-        order: [
-          'DEFINITION',
-          'LIST_RENDERING',
-          'CONDITIONALS',
-          'RENDER_MODIFIERS',
-          'GLOBAL',
-          ['UNIQUE', 'SLOT'],
-          'TWO_WAY_BINDING',
-          'OTHER_DIRECTIVES',
-          ['ATTR_STATIC', 'ATTR_SHORTHAND_BOOL'],
-          'ATTR_DYNAMIC',
-          'EVENTS',
-          'CONTENT'
+    root: true,
+    extends: [
+        'plugin:vue/vue3-recommended',
+        'eslint:recommended',
+        'plugin:storybook/recommended',
+        '@vue/eslint-config-prettier'
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest'
+    },
+    env: {
+        browser: true,
+        amd: true,
+        node: true
+    },
+    rules: {
+        'vue/html-indent': ['error', 4],
+        eqeqeq: ['error', 'always'],
+        'vue/eqeqeq': ['error', 'always'],
+        'prettier/prettier':
+            process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+        'no-unused-vars':
+            process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+        'vue/no-unused-vars':
+            process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+        'vue/attributes-order': [
+            'warn',
+            {
+                order: [
+                    'DEFINITION',
+                    'LIST_RENDERING',
+                    'CONDITIONALS',
+                    'RENDER_MODIFIERS',
+                    'GLOBAL',
+                    ['UNIQUE', 'SLOT'],
+                    'TWO_WAY_BINDING',
+                    'OTHER_DIRECTIVES',
+                    ['ATTR_STATIC', 'ATTR_SHORTHAND_BOOL'],
+                    'ATTR_DYNAMIC',
+                    'EVENTS',
+                    'CONTENT'
+                ],
+                alphabetical: true
+            }
         ],
-        alphabetical: true
-      }
-    ],
-    'vue/html-self-closing': [
-      'warn',
-      {
-        html: { void: 'always', normal: 'always', component: 'always' },
-        svg: 'always',
-        math: 'always'
-      }
-    ],
-  }
+        'vue/html-self-closing': [
+            'warn',
+            {
+                html: { void: 'always', normal: 'always', component: 'always' },
+                svg: 'always',
+                math: 'always'
+            }
+        ]
+    }
 }
