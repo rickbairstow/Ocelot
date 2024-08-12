d6a828c225d4a294e39d95f7e99f54890c7aa7f0
Rick <44267427+rickbairstow@users.noreply.github.com>
2024-08-12 11:48:44 +0100

    Add initial rule overrides

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,5 +13,43 @@ module.exports = {
     browser: true,
     amd: true,
     node: true
+  },
+  rules: {
+    eqeqeq: ['error', 'always'],
+    'vue/eqeqeq': ['error', 'always'],
+    'prettier/prettier':
+      process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-unused-vars':
+      process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'vue/no-unused-vars':
+      process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'vue/attributes-order': [
+      'warn',
+      {
+        order: [
+          'DEFINITION',
+          'LIST_RENDERING',
+          'CONDITIONALS',
+          'RENDER_MODIFIERS',
+          'GLOBAL',
+          ['UNIQUE', 'SLOT'],
+          'TWO_WAY_BINDING',
+          'OTHER_DIRECTIVES',
+          ['ATTR_STATIC', 'ATTR_SHORTHAND_BOOL'],
+          'ATTR_DYNAMIC',
+          'EVENTS',
+          'CONTENT'
+        ],
+        alphabetical: true
+      }
+    ],
+    'vue/html-self-closing': [
+      'warn',
+      {
+        html: { void: 'always', normal: 'always', component: 'always' },
+        svg: 'always',
+        math: 'always'
+      }
+    ],
   }
 }
