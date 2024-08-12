b3fe9df92c5bd38e00b8a1a50fce428776bba0be
Rick <44267427+rickbairstow@users.noreply.github.com>
2024-08-12 11:44:14 +0100

    Add patch for prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,7 @@ module.exports = {
     'plugin:vue/vue3-recommended',
     'eslint:recommended',
     'plugin:storybook/recommended',
-    'prettier'
+    "@vue/eslint-config-prettier"
   ],
   parserOptions: {
     ecmaVersion: 'latest'
