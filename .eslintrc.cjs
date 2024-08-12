a833b623a9a17f26318da48fdabbb4dcf577ac64
Rick <44267427+rickbairstow@users.noreply.github.com>
2024-08-12 11:41:32 +0100

    Update ESLint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,17 @@
 module.exports = {
   root: true,
   extends: [
-    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-recommended',
     'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting',
-    'plugin:storybook/recommended'
+    'plugin:storybook/recommended',
+    'prettier'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  env: {
+    browser: true,
+    amd: true,
+    node: true
   }
 }
