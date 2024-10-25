a2c92e5921c4c7147c59f5cca18ac711dd58426c
rickbairstow <44267427+rickbairstow@users.noreply.github.com>
2024-10-25 20:24:42 +0100

    Add dialog component

---
+++ src/components/Dialog.vue
@@ -0,0 +1,164 @@
+<template>
+    <Teleport :to="portalTarget">
+        <Transition>
+            <!-- eslint-disable-next-line vuejs-accessibility/no-static-element-interactions -->
+            <section
+                v-if="isOpen"
+                class="fixed inset-0 z-[60] flex items-center justify-center sm:p-6"
+                role="complementary"
+                @keydown.esc="close"
+            >
+                <button
+                    id="backdrop"
+                    aria-label="Close dialog"
+                    class="fixed inset-0 opacity-90 bg-gray-500"
+                    @click="close"
+                />
+
+                <div
+                    class="relative flex flex-col justify-between max-h-full h-full overflow-hidden text-black bg-white z-10 sm:h-auto sm:rounded-2xl"
+                    role="dialog"
+                    :aria-label="ariaLabel"
+                    :class="sizeClass"
+                >
+                    <div
+                        class="flex items-center justify-between border-gray-300 border-b"
+                    >
+                        <div
+                            id="dialogueTitle"
+                            class="flex p-6"
+                            tabindex="0"
+                        >
+                            <slot name="title" />
+                        </div>
+
+                        <Button
+                            aria-label="Close dialog"
+                            class="mr-2"
+                            type="none"
+                            @click="close"
+                        >
+                            <Icon icon="X" />
+                        </Button>
+                    </div>
+
+                    <div
+                        ref="dialogueContent"
+                        class="p-6 bg-white overflow-auto"
+                        tabindex="0"
+                    >
+                        <slot name="default" />
+                    </div>
+
+                    <div
+                        v-if="slots?.footer"
+                        class="flex p-6 border-t border-gray-300"
+                    >
+                        <slot name="footer" />
+                    </div>
+
+                    <Button
+                        aria-label="Close dialog"
+                        button-type="tertiary"
+                        class="sr-only"
+                        @click="close"
+                    >
+                        Close {{ ariaLabel }}
+                    </Button>
+                </div>
+            </section>
+        </Transition>
+    </Teleport>
+</template>
+
+<script setup>
+import { computed, ref, useSlots, watch } from 'vue'
+import Button from '@Components/Button.vue'
+import Icon from '@Components/Icon.vue'
+
+const slots = useSlots()
+const dialogueContent = ref(null)
+
+const props = defineProps({
+    ariaLabel: {
+        required: true,
+        type: String
+    },
+
+    isOpen: {
+        type: Boolean,
+        default: false
+    },
+
+    focusFrom: {
+        default: null,
+        type: String // ID of element
+    },
+
+    focusTo: {
+        default: null,
+        type: String // ID of element
+    },
+
+    portalTarget: {
+        default: '#portal-target',
+        type: String
+    },
+
+    small: {
+        default: true,
+        type: Boolean
+    }
+})
+
+const emit = defineEmits(['close'])
+
+/**
+ * Emits a close event.
+ */
+const close = () => {
+    emit('close')
+}
+
+/**
+ * Check if an id resolves to a valid DOM element.
+ * @param {string} id
+ * @returns {HTMLElement|null}
+ */
+const getEl = (id) => document.getElementById(id) ?? null
+
+/**
+ * Triggers focus on a given element.
+ * @param {HTMLElement} element
+ * @returns {*}
+ */
+const setFocus = (element) => element && element?.focus()
+
+/**
+ * Calculate size constraints.
+ */
+const sizeClass = computed(() => {
+    const { small } = props
+    return `${small ? 'w-80' : 'w-full'} max-w-full`
+})
+
+/**
+ * Handles focus events with 1ms timeout for teleport functionality.
+ */
+watch(
+    () => props.isOpen,
+    (val) => {
+        const { focusTo, focusFrom } = props
+
+        if (val && focusTo)
+            setTimeout(() => {
+                setFocus(getEl(focusTo))
+            }, 50)
+
+        if (val === false && focusFrom)
+            setTimeout(() => {
+                setFocus(getEl(focusFrom))
+            }, 50)
+    }
+)
+</script>
---
+++ src/stories/components/Dialog.stories.js
@@ -0,0 +1,70 @@
+import Dialog from '@Components/Dialog.vue'
+
+export default {
+    title: 'Components/Dialog',
+    component: Dialog,
+
+    argTypes: {},
+    args: {
+        // slot content
+        default: 'Dialog content',
+        defaultFooter: 'Dialog footer',
+        defaultTitle: 'Dialog title',
+
+        // props
+        ariaLabel: 'My dialog',
+        focusFrom: 'dialogueTrigger',
+        focusTo: 'dialogueTrigger',
+        isOpen: false,
+        small: true
+    },
+
+    render: (args) => ({
+        components: { Dialog },
+
+        setup() {
+            const close = () => {
+                args.isOpen = false
+            }
+
+            const open = () => {
+                args.isOpen = true
+            }
+
+            return { args, close, open }
+        },
+
+        template: `
+            <div id="portal-target"></div>
+
+            <button
+                id="dialogueTrigger"
+                @click="open()"
+            >
+                Click to trigger dialog.
+            </button>
+
+            <Dialog
+                :aria-label="args.ariaLabel"
+                :focus-from="args.focusFrom"
+                :focus-to="args.focusTo"
+                :is-open="args.isOpen"
+                :portal-target="args.portalTarget"
+                :small="args.small"
+                @close="close()"
+            >
+                <template #title>
+                    {{ args.defaultTitle }}
+                </template>
+
+                {{ args.default }}
+
+                <template #footer>
+                    {{ args.defaultFooter }}
+                </template>
+            </Dialog>
+        `
+    })
+}
+
+export const DialogStory = {}
