d02fce067ba492463d6c620dbf036bf9341d99f7
rickbairstow <44267427+rickbairstow@users.noreply.github.com>
2024-10-27 20:02:08 +0000

    Apply slide transitions to sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,57 +1,55 @@
 <template>
-    <Transition>
+    <Transition :name="side === 'left' ? 'slide-left' : 'slide-right'">
         <!-- eslint-disable-next-line vuejs-accessibility/no-static-element-interactions -->
         <div
             v-if="isOpen"
+            class="fixed inset-y-0 z-20 flex flex-col w-full sm:w-64 max-h-full max-w-full bg-white text-black"
+            :class="sideClasses"
             @keydown.esc="close()"
         >
             <div
-                class="fixed inset-y-0 z-20 flex flex-col w-full sm:w-64 max-h-full max-w-full bg-white text-black"
-                :class="sideClasses"
+                v-if="slots?.title"
+                class="flex-none flex items-center justify-between gap-4 border-b py-2 bg-gray-100"
             >
-                <div
-                    v-if="slots?.title"
-                    class="flex-none flex items-center justify-between gap-4 border-b py-2 bg-gray-100"
-                >
-                    <div class="px-4">
-                        <slot name="title" />
-                    </div>
-
-                    <Button
-                        aria-label="Close sidebar"
-                        type="none"
-                        @click="close()"
-                    >
-                        <Icon icon="X" />
-                    </Button>
+                <div class="px-4">
+                    <slot name="title" />
                 </div>
 
-                <div class="flex-1 overflow-y-auto">
-                    <slot />
-                </div>
-
-                <div
-                    v-if="slots?.footer"
-                    class="flex-none border-t p-4 bg-gray-100"
-                >
-                    <slot name="footer" />
-                </div>
-
-                <button
+                <Button
                     aria-label="Close sidebar"
-                    class="sr-only"
+                    type="none"
                     @click="close()"
-                />
+                >
+                    <Icon icon="X" />
+                </Button>
             </div>
 
-            <Scrim
-                v-if="isOpen && showScrim"
+            <div class="flex-1 overflow-y-auto">
+                <slot />
+            </div>
+
+            <div
+                v-if="slots?.footer"
+                class="flex-none border-t p-4 bg-gray-100"
+            >
+                <slot name="footer" />
+            </div>
+
+            <button
                 aria-label="Close sidebar"
-                class="z-0"
+                class="sr-only"
                 @click="close()"
             />
         </div>
     </Transition>
+    <transition>
+        <Scrim
+            v-if="isOpen && showScrim"
+            aria-label="Close sidebar"
+            class="z-0"
+            @click="close()"
+        />
+    </transition>
 </template>
 
 <script setup>
