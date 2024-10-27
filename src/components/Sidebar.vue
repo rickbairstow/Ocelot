1c792c60d8b8a39f26cedfad78f628900927ee6c
rickbairstow <44267427+rickbairstow@users.noreply.github.com>
2024-10-27 19:36:52 +0000

    Add sidebar component

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,97 @@
+<template>
+    <Transition>
+        <!-- eslint-disable-next-line vuejs-accessibility/no-static-element-interactions -->
+        <div
+            v-if="isOpen"
+            @keydown.esc="close()"
+        >
+            <div
+                class="fixed inset-y-0 z-20 flex flex-col w-full sm:w-64 max-h-full max-w-full bg-white text-black"
+                :class="sideClasses"
+            >
+                <div
+                    v-if="slots?.title"
+                    class="flex-none flex items-center justify-between gap-4 border-b py-2 bg-gray-100"
+                >
+                    <div class="px-4">
+                        <slot name="title" />
+                    </div>
+
+                    <Button
+                        aria-label="Close sidebar"
+                        type="none"
+                        @click="close()"
+                    >
+                        <Icon icon="X" />
+                    </Button>
+                </div>
+
+                <div class="flex-1 overflow-y-auto">
+                    <slot />
+                </div>
+
+                <div
+                    v-if="slots?.footer"
+                    class="flex-none border-t p-4 bg-gray-100"
+                >
+                    <slot name="footer" />
+                </div>
+
+                <button
+                    aria-label="Close sidebar"
+                    class="sr-only"
+                    @click="close()"
+                />
+            </div>
+
+            <Scrim
+                v-if="isOpen && showScrim"
+                aria-label="Close sidebar"
+                class="z-0"
+                @click="close()"
+            />
+        </div>
+    </Transition>
+</template>
+
+<script setup>
+import { computed, ref, useSlots } from 'vue'
+import Button from '@Components/Button.vue'
+import Icon from '@Components/Icon.vue'
+import Scrim from '@Components/Scrim.vue'
+
+const props = defineProps({
+    showScrim: {
+        default: true,
+        type: Boolean
+    },
+    side: {
+        default: 'left',
+        type: String,
+        validator(value) {
+            return ['left', 'right'].includes(value)
+        }
+    }
+})
+
+const slots = useSlots()
+const isOpen = ref(false)
+const open = () => (isOpen.value = true)
+const close = () => (isOpen.value = false)
+
+defineExpose({
+    close,
+    isOpen,
+    open
+})
+
+const sideClasses = computed(() => {
+    const { side } = props
+
+    if (side === 'left') {
+        return 'left-0 border-r'
+    }
+
+    return 'right-0 border-l'
+})
+</script>
