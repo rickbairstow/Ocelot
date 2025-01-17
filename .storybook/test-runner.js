3c8baf02b1a6bb624c1c75a1c6bf4628fe7d383d
rickbairstow <44267427+rickbairstow@users.noreply.github.com>
2025-01-17 17:28:30 +0000

    Revert "Update test-runner.js"
    This reverts commit 2de66cae25b70a67b877281ba261f27da44a3b00.

--- .storybook/test-runner.js
+++ .storybook/test-runner.js
@@ -1,7 +1,5 @@
 const { injectAxe, checkA11y } = require('axe-playwright')
 
-let isAxeRunning = false;
-
 /*
  * See https://storybook.js.org/docs/writing-tests/test-runner#test-hook-api
  * to learn more about the test-runner hooks API.
@@ -11,32 +9,19 @@ module.exports = {
         await injectAxe(page)
     },
     async postVisit(page) {
-        while (isAxeRunning) {
-            await new Promise((resolve) => setTimeout(resolve, 50)) // Wait until the previous Axe run finishes
-        }
-        isAxeRunning = true
-        try {
-            await checkA11y(
-                page,
-                '#storybook-root',
-                {
-                    detailedReport: true,
-                    detailedReportOptions: {
-                        html: true
-                    },
-                    includedImpacts: [
-                        'minor',
-                        'moderate',
-                        'serious',
-                        'critical'
-                    ],
-                    verbose: false
+        await checkA11y(
+            page,
+            '#storybook-root',
+            {
+                detailedReport: true,
+                detailedReportOptions: {
+                    html: true
                 },
-                false,
-                'v2'
-            )
-        } finally {
-            isAxeRunning = false
-        }
+                includedImpacts: ['minor', 'moderate', 'serious', 'critical'],
+                verbose: false
+            },
+            false,
+            'v2'
+        )
     }
 }
