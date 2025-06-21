d412420f4d64858e68c86732a8558c2bd3e96a40
rickbairstow <44267427+rickbairstow@users.noreply.github.com>
2025-06-21 21:26:23 +0100

    Update Button.vue

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,10 +1,10 @@
 <template>
     <component
         :is="element"
-        role="button"
         :aria-disabled="disabled ? 'true' : undefined"
         :class="componentStyle"
         :href="href"
+        :role="href ? 'link' : 'button'"
         @click="handleInteraction"
         @keydown.enter.prevent="handleInteraction"
         @keydown.space.prevent="handleInteraction"
@@ -59,31 +59,26 @@ const styles = {
         base: 'py-2 px-3',
         large: 'py-3 px-4'
     },
-    common: 'inline-flex items-center justify-center border rounded-lg transition-colors cursor-pointer focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500',
+    common: 'inline-flex items-center justify-center border rounded-lg transition-colors focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500',
     primary: {
         base: 'text-white bg-blue-600 border-blue-400 hover:bg-blue-700',
-        disabled:
-            'text-white bg-gray-400 border-gray-400 pointer-events-none opacity-50'
+        disabled: 'text-white bg-gray-400 border-gray-400 opacity-50'
     },
     secondary: {
         base: 'text-black bg-white border-gray-200 hover:bg-gray-200',
-        disabled:
-            'text-black bg-gray-100 border-gray-100 pointer-events-none opacity-50'
+        disabled: 'text-black bg-gray-100 border-gray-100 opacity-50'
     },
     tertiary: {
         base: 'text-black bg-transparent border-transparent hover:bg-gray-200',
-        disabled:
-            'text-gray-900 bg-transparent border-transparent pointer-events-none opacity-50'
+        disabled: 'text-gray-900 bg-transparent border-transparent opacity-50'
     },
     text: {
         base: 'text-blue-600 bg-transparent border-transparent hover:text-blue-700 hover:underline',
-        disabled:
-            'text-blue-900 bg-transparent border-transparent pointer-events-none opacity-50'
+        disabled: 'text-blue-900 bg-transparent border-transparent opacity-50'
     },
     none: {
         base: 'text-black bg-transparent border-transparent',
-        disabled:
-            'text-gray-900 bg-transparent border-transparent pointer-events-none opacity-50'
+        disabled: 'text-gray-900 bg-transparent border-transparent opacity-50'
     }
 }
 
@@ -93,7 +88,8 @@ const styles = {
  */
 const componentStyle = computed(() => {
     const { size, type, disabled } = props
+    const interactive = disabled ? 'cursor-not-allowed' : 'cursor-pointer'
     const visual = styles[type]?.[disabled ? 'disabled' : 'base'] || ''
-    return `${styles.common} ${styles.padding[size]} ${visual}`
+    return `${styles.common} ${styles.padding[size]} ${visual} ${interactive}`
 })
 </script>
