70119486fc6e95766f84f4fcc6ded66a665732f1
rickbairstow <44267427+rickbairstow@users.noreply.github.com>
2025-06-21 22:22:26 +0100

    Update ReadMore.stories.js

--- src/stories/components/ReadMore.stories.js
+++ src/stories/components/ReadMore.stories.js
@@ -11,7 +11,6 @@ export default {
             control: 'text',
             description: 'Slot content'
         },
-
         lines: {
             control: {
                 type: 'number',
@@ -22,37 +21,35 @@ export default {
     },
 
     args: {
-        default: faker.lorem.paragraphs(1, '\n\n'),
+        default: faker.lorem.paragraphs(8, '\n\n'),
         lines: 4
-    }
-}
+    },
 
-export const Default = {
     render: (args) => ({
         components: { ReadMore },
         setup() {
             return { args }
         },
         template: `
-            <ReadMore :lines="args.lines">
-                {{ args.default }}
-            </ReadMore>
+            <div style="width: 300px">
+                <ReadMore :lines="args.lines">
+                    {{ args.default }}
+                </ReadMore>
+            </div>
         `
-    }),
+    })
+}
 
+export const Default = {
     play: async ({ canvasElement }) => {
         const canvas = within(canvasElement)
 
         const button = await canvas.findByRole('button', { name: /show more/i })
         await expect(button).toBeVisible()
 
-        // Click to expand
         await userEvent.click(button)
-
-        // Button should now read "Show less"
         await expect(button).toHaveTextContent(/show less/i)
 
-        // Click again to collapse
         await userEvent.click(button)
         await expect(button).toHaveTextContent(/show more/i)
     }
