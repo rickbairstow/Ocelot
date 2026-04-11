25105bc9ff5ddd5b3d78cab5eb8a48210339ab06
rickbairstow <44267427+rickbairstow@users.noreply.github.com>
2026-04-11 17:34:01 +0100

    Better darkmode

--- src/components/Scrim.vue
+++ src/components/Scrim.vue
@@ -1,7 +1,7 @@
 <template>
     <component
         :is="clickable ? 'button' : 'div'"
-        class="inset-0 bg-slate-600 opacity-90 z-0"
+        class="inset-0 bg-slate-600 dark:bg-slate-900 opacity-90 z-0"
         :aria-disabled="!clickable ? 'true' : undefined"
         :aria-label="clickable && ariaLabel ? ariaLabel : undefined"
         :class="[
